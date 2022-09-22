11bd12481e9ddaa5dd6697fe85ef531281b6285e
Simon Engwerda <simon@engwerda.net>
2022-09-22 15:55:52 +0700

    Clean up cmp and lsp config.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -95,9 +95,6 @@ return packer.startup(function(use)
   use("Mofiqul/dracula.nvim")
   use("lunarvim/darkplus.nvim")
 
-  -- Motion
-  use ({ "phaazon/hop.nvim", branch = 'v2', config = get_config("hop") })
-
 
   -- Copilot
   use({ "github/copilot.vim", config = get_config("copilot") })
@@ -109,29 +106,6 @@ return packer.startup(function(use)
   use({ "rafamadriz/friendly-snippets", commit = "d27a83a363e61009278b6598703a763ce9c8e617" }) -- a bunch of snippets to use
   use({ "honza/vim-snippets" })
 
-  -- cmp plugins
-  -- use({ "hrsh7th/nvim-cmp", config = get_config("cmp"), commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
-  -- use({ "hrsh7th/cmp-buffer", commit = "62fc67a2b0205136bc3e312664624ba2ab4a9323" }) -- buffer completions
-  -- use({ "hrsh7th/cmp-path", commit = "466b6b8270f7ba89abd59f402c73f63c7331ff6e" }) -- path completions
-  -- use({ "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" }) -- snippet completions
-  -- use({ "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" })
-  -- use({ "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" })
-  --  use { "tzachar/cmp-tabnine",config = get_config("tabnine"), after = "nvim-cmp", run="./install.sh" }
-
-  -- cmp snippets
-  -- use({ "L3MON4D3/LuaSnip", commit = "79b2019c68a2ff5ae4d732d50746c901dd45603a" }) --snippet engine
-  -- use({ "rafamadriz/friendly-snippets", commit = "d27a83a363e61009278b6598703a763ce9c8e617" }) -- a bunch of snippets to use
-
-  -- LSP
-  -- use({ "neovim/nvim-lspconfig", config = get_config("lsp/init"), commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" }) -- enable LSP
-  -- use({ "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" }) -- simple to use language server installer
-  -- use({ "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" }) -- for formatters and linters
-  --
-  --  use({
-  --    "williamboman/mason.nvim",
-  --    requires = { "williamboman/mason-lspconfig.nvim", "WhoIsSethDaniel/mason-tool-installer.nvim" },
-  --    config = get_config("mason"),
-  --  })
 
   -- Telescope
   use({ "nvim-telescope/telescope.nvim", config = get_config("telescope"),
