b6c0a81f1d1ff72b57ced3a7745549ebd5a7855c
Simon Engwerda <simon@engwerda.net>
2022-09-23 20:23:50 +0700

    Add octo plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -144,6 +144,17 @@ return packer.startup(function(use)
     config = function() require('user.config.neogit').setup() end
   })
   use({ "kdheepak/lazygit.nvim" })
+  use({
+    'pwntester/octo.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'kyazdani42/nvim-web-devicons',
+    },
+    config = function()
+      require "octo".setup()
+    end
+  })
 
   -- DAP
   use({
