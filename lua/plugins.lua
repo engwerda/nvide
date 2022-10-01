d2a1c5f75ccee35170c044e6c7d0bebeaf7c6fec
Simon Engwerda <simon@engwerda.net>
2022-10-01 14:22:47 +0700

    Update plugins to latests version

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,48 +58,43 @@ return packer.startup(function(use)
 		end,
 	})
 
-	use({ "wbthomason/packer.nvim", commit = "00ec5adef58c5ff9a07f11f45903b9dbbaa1b422" }) -- Have packer manage itself
-	use({ "nvim-lua/plenary.nvim", commit = "968a4b9afec0c633bc369662e78f8c5db0eba249" }) -- Useful lua functions used by lots of plugins
+	use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
+	use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
 	use({
 		"windwp/nvim-autopairs",
 		config = get_config("autopairs"),
-		commit = "fa6876f832ea1b71801c4e481d8feca9a36215ec",
 	}) -- Autopairs, integrates with both cmp and treesitter
-	use({ "numToStr/Comment.nvim", config = get_config("comment"), commit = "2c26a00f32b190390b664e56e32fd5347613b9e2" })
-	use({ "JoosepAlviste/nvim-ts-context-commentstring", commit = "88343753dbe81c227a1c1fd2c8d764afb8d36269" })
-	use({ "kyazdani42/nvim-web-devicons", commit = "8d2c5337f0a2d0a17de8e751876eeb192b32310e" })
+	use({ "numToStr/Comment.nvim", config = get_config("comment") })
+	use({ "JoosepAlviste/nvim-ts-context-commentstring" })
+	use({ "kyazdani42/nvim-web-devicons" })
 	use({
 		"kyazdani42/nvim-tree.lua",
 		config = get_config("nvim-tree"),
-		commit = "bdb6d4a25410da35bbf7ce0dbdaa8d60432bc243",
 	})
 	use({
 		"akinsho/bufferline.nvim",
 		config = get_config("bufferline"),
-		commit = "c78b3ecf9539a719828bca82fc7ddb9b3ba0c353",
+    tag = "v2.*",
+    requires = 'kyazdani42/nvim-web-devicons'
 	})
-	use({ "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" })
+	use({ "moll/vim-bbye" })
 	use({
 		"nvim-lualine/lualine.nvim",
 		config = get_config("lualine"),
-		commit = "3362b28f917acc37538b1047f187ff1b5645ecdd",
 	})
 	use({
 		"ahmedkhalf/project.nvim",
 		config = get_config("project"),
-		commit = "541115e762764bc44d7d3bf501b6e367842d3d4f",
 	})
 	use({
 		"lewis6991/impatient.nvim",
 		config = get_config("impatient"),
-		commit = "969f2c5c90457612c09cf2a13fee1adaa986d350",
 	})
 	use({
 		"lukas-reineke/indent-blankline.nvim",
 		config = get_config("indent-blankline"),
-		commit = "6177a59552e35dfb69e1493fd68194e673dc3ee2",
 	})
-	use({ "goolord/alpha-nvim", config = get_config("alpha-nvim"), commit = "ef27a59e5b4d7b1c2fe1950da3fe5b1c5f3b4c94" })
+	use({ "goolord/alpha-nvim", config = get_config("alpha-nvim") })
 	use({ "folke/which-key.nvim", config = get_config("whichkey") })
 
 	use({
@@ -135,7 +130,6 @@ return packer.startup(function(use)
 	use({
 		"akinsho/toggleterm.nvim",
 		config = get_config("toggleterm"),
-		commit = "aaeed9e02167c5e8f00f25156895a6fd95403af8",
 	})
 	use({
 		"numToStr/Navigator.nvim",
@@ -146,7 +140,7 @@ return packer.startup(function(use)
 
 	-- Colorschemes
 
-	use({ "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" })
+	use({ "folke/tokyonight.nvim" })
 	use("lunarvim/colorschemes")
 	use("Mofiqul/dracula.nvim")
 	use("lunarvim/darkplus.nvim")
@@ -164,14 +158,13 @@ return packer.startup(function(use)
 	})
 
 	-- coc snippets
-	use({ "rafamadriz/friendly-snippets", commit = "d27a83a363e61009278b6598703a763ce9c8e617" }) -- a bunch of snippets to use
+	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 	use({ "honza/vim-snippets" })
 
 	-- Telescope
 	use({
 		"nvim-telescope/telescope.nvim",
 		config = get_config("telescope"),
-		commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0",
 	})
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 	use({ "nvim-telescope/telescope-file-browser.nvim" })
@@ -189,14 +182,12 @@ return packer.startup(function(use)
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		config = get_config("treesitter"),
-		commit = "518e27589c0463af15463c9d675c65e464efc2fe",
 	})
 
 	-- Git
 	use({
 		"lewis6991/gitsigns.nvim",
 		config = get_config("gitsigns"),
-		commit = "c18e016864c92ecf9775abea1baaa161c28082c3",
 	})
 	use({
 		"TimUntersberger/neogit",
